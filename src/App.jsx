eb3f19c42479e505ea4d209f3f1cdfbaee5ee122
SMCominotti <smcominotti@gmail.com>
2024-04-05 00:36:33 +0200

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import {
   RiPieChartLine,
   RiMenu3Fill,
   RiUser3Line,
-  RiCloseLine
+  RiCloseLine,
+  RiSearch2Line,
+  RiArrowDownSLine
 } from "react-icons/ri";
 import Sidebar from "./components/shared/Sidebar";
 
@@ -34,6 +36,37 @@ function App() {
           {showMenu ? <RiCloseLine/> : <RiMenu3Fill />}
         </button>
       </nav>
+      <main className="lg:pl-28 grid grid-cols-1 lg:grid-cols-8">
+        <div className="lg:col-span-6">
+        <header className="p-4">
+          <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-4 mb-6">
+          <div>
+            <h1 className="text-2xl text-gray-300">Home Food</h1>
+            <p className="text-gray-500">04 Abril 2024</p>
+          </div>
+          <form>
+            <div className="w-full relative">
+            <RiSearch2Line className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-300"/>
+              <input type="text" className="bg-[#1F1D2B] w-full py-2 pl-10 pr-4 rounded-lg text-gray-300 outline-none" placeholder="Search"/>
+            </div>
+          </form>
+        </div>  
+        <nav className="text-gray-300 flex item-center justify-between md:justify-start md:gap-8 border-b border-gray-600 mb-6">
+          <a href="#" className="relative py-2 pr-4 before:w-1/2 before:h-1 before:absolute before:bg-[#ec7c6a] before:left-0 before:rounded-full before:-bottom-[2px] text-[#ec7c6a]">Hot Dishes</a>
+          <a href="#"className="py-2 pr-4">Cold Dishes</a>
+          <a href="#"className="py-2 pr-4">Grill</a>
+          <a href="#"className="py-2 pr-4">Dessert</a>
+        </nav>
+        <div className=" flex items-center justify-between">
+          <h2 className="text-xl text-gray-300">Choose Dishes</h2>
+          <button className="flex items-center gap-4 text-gray-300 bg-[#1F1D2B] py-2 px-4 rounded-lg">
+            <RiArrowDownSLine/> Dine in
+            </button>
+        </div>
+        </header>
+        </div>
+        <div className="lg:col-span-2 fixed lg:static right-0">Carrito</div>
+      </main>
     </h1>
   );
 }
